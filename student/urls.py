14aa2bee196c3950a697cccde1d9e8a7aef95409
baron <baron@qq.com>
2019-07-15 18:59:16 +0800

    增加登录页面

--- student/urls.py
+++ student/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 
 
     url('student/',student,name='stu_index'),#为url起了个别名，改这里的url，前端不用改变
-
+    url('my_course/',my_course,name='my_course'),
 
 ]
--- student/views.py
+++ student/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def student(request):
     return render(request,'student/student.html')
+
+def my_course(request):
+    return render(request,'student/my_course.html')
